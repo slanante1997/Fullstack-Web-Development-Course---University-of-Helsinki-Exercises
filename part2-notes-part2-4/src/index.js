5fd0d2d0506c29846928bd894201f16663641554
Seth Lanante <slanante@commerce-architects.com>
2023-04-21 11:25:47 -0700

    Continued part3

--- part2-notes-part2-4/src/index.js
+++ part2-notes-part2-4/src/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 import App from './App'
 
-axios.get('http://localhost:3001/notes').then(response => {
+axios.get('http://localhost:3001/api/notes').then(response => {
   const notes = response.data
   ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
 })
